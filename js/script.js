6d2931568dcb0ae44803f6c98d5341fe7e1e9f67
VladimirIanchenkov <ianchenkov.v@gmail.com>
2022-11-09 18:44:19 +0700

    выполнил сортировку

--- js/script.js
+++ js/script.js
@@ -1,22 +1,7 @@
-// const GET_DATA_SERVER_LINK = 'https://jsonplaceholder.typicode.com/users';
-
-// const getData = (onSuccess, onFail) => {
-//   fetch(GET_DATA_SERVER_LINK)
-//     .then((response) => {
-//       if (response.ok) {
-//         return response.json();
-//       }
-//       throw new Error(`${response.status} ${response.statusText}`);
-//     })
-//     .then((cards) => {
-//       onSuccess(cards);
-//     })
-//     .catch(() => {
-//       onFail();
-//     });
-// };
 const table = document.querySelector('.table');
+const sorters = document.querySelector('.table__heading').querySelectorAll('input');
 
+// Функия, создающая карточку клиента
 const createCustomCard = (card) => {
   const cardItemTemplate = document.querySelector('#card').content.querySelector('.table__row');
   const cardItem = cardItemTemplate.cloneNode(true);
@@ -26,20 +11,123 @@ const createCustomCard = (card) => {
   cardItem.querySelector('.table__tel').textContent = card.phone;
   cardItem.querySelector('.table__company').textContent = card.company.name;
 
-  table.appendChild(cardItem);
-  console.log(table);
   return cardItem;
 }
 
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then((response) => response.json())
-  .then((cards) => {
-    cards.forEach((card) => createCustomCard(card));
-    console.log(cards);
+const compareNames = (a, b) => {
+  if (a.name > b.name) {
+    return 1;
   }
-);
+  if (a.name < b.name) {
+    return -1;
+  }
+  return 0;
+}
 
+const compareLinks = (a, b) => {
+  if (a.email > b.email) {
+    return 1;
+  }
+  if (a.email < b.email) {
+    return -1;
+  }
+  return 0;
+}
 
+const compareAddresses = (a, b) => {
+  if (a.address.city > b.address.city) {
+    return 1;
+  }
+  if (a.address.city < b.address.city) {
+    return -1;
+  }
+  return 0;
+}
 
+const comparePhones = (a, b) => {
+  if (a.phone > b.phone) {
+    return 1;
+  }
+  if (a.phone < b.phone) {
+    return -1;
+  }
+  return 0;
+}
 
+const compareCompanies = (a, b) => {
+  if (a.company.name > b.company.name) {
+    return 1;
+  }
+  if (a.company.name < b.company.name) {
+    return -1;
+  }
+  return 0;
+}
 
+const sortOptions = {
+  'name' : compareNames,
+  'email' : compareLinks,
+  'address' : compareAddresses,
+  'phone' : comparePhones,
+  'company' : compareCompanies,
+}
+
+const createDefaultList = (cards) => {
+  cards.forEach((card) => table.appendChild(createCustomCard(card)));
+}
+
+const createSortedList = (cards, sorter) => {
+  document.querySelectorAll('.table__row').forEach((elem) => elem.parentNode.removeChild(elem));
+  cards.slice().sort(sortOptions[sorter]).forEach((card) => table.appendChild(createCustomCard(card)));
+}
+
+const GET_DATA_SERVER_LINK = 'https://jsonplaceholder.typicode.com/users';
+
+// Отрисовка ошибки загрузки данных с сервера
+const showServerAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '20px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.color = '#ffffff';
+  alertContainer.style.backgroundColor = '#ff5635';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+};
+
+// Функция загрузки данных с сервера
+const getData = (onSuccess, onFail) => {
+  fetch(GET_DATA_SERVER_LINK)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(`${response.status} ${response.statusText}`);
+    })
+    .then((cards) => {
+      onSuccess(cards);
+    })
+    // .catch(() => {
+    //   onFail();
+    // });
+};
+
+getData(
+  (cards) => {
+    createDefaultList(cards);
+    sorters.forEach((el) => el.addEventListener('click', () => {
+      console.log(el.id);
+      createSortedList(cards, el.id);
+    }));
+  },
+  () => {
+    showServerAlert('Не удалось загрузить данные о пользователях с сервера. Попробуйте обновить страницу');
+  },
+)
